6cd3a978d1189cf12dae966c18d577edc989a187
nicolascorderodev <nicolascordero281@gmail.com>
2024-10-12 15:05:54 -0500

    Avanzando a sintaxis

---
+++ python/hola_mundo.py
@@ -0,0 +1,23 @@
+#EJERCICIO 1 HOLA MUNDO
+print("Hola mundo")#Función print
+
+x = 5#Creando una variable tipo int
+print(x)#Imprimir variable con la función print
+
+#SUMANDO VARIABLES EN PYTHON
+#Declarando las variables a, b y asignamos dos valores.
+a = 3
+b = 7
+
+print(a+b)#Usamos la función print para imprimir la suma de las dos variables(importante usar variables que hayan sido definidas.)
+
+#EJEMPLO CONDICIONAL
+a = 10
+if a == 10:#Uso de una sentencia condicional para comprobar si la variable a es igual a 10. Si lo es, se imprimirá "Es 10" y si no lo es "No es 10." Es importante el uso de = ya que es el operador relacional.
+    print("Es 10.")
+else:
+    print("No es 10.")
+
+#DECIMALES Y CADENAS
+valor_decimal = 10.3234
+mi_cadena = "Esto es un string."
